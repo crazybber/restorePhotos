774568e3b090ad799816f14607aa2181defec58d
Edward <crazybber@outlook.com>
2023-05-10 18:33:13 +0800

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,5 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
+import GitHub from 'next-auth/providers/github'
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "../../../lib/prismadb";
@@ -6,6 +7,10 @@ import prisma from "../../../lib/prismadb";
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
