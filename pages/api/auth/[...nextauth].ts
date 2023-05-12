ea9ac63d2a79604b9fcb9f8880760844d5f71292
Edward <crazybber@outlook.com>
2023-05-12 06:54:56 +0000

    comment google provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,6 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GitHub from 'next-auth/providers/github'
-import GoogleProvider from "next-auth/providers/google";
+//import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "../../../lib/prismadb";
 
